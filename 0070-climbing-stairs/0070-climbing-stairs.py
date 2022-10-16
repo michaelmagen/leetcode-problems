53ec70ae92cbc6d6260d02265e1b2e8034845c96
michaelmagen <107211477+michaelmagen@users.noreply.github.com>
2022-10-16 16:13:15 -0700

    Time: 33 ms (90.33%), Space: 13.8 MB (57.10%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # like fibonacci. to get number of paths n, take (n - 1) + (n - 2)
         one, two = 1, 1
         
         for i in range(n - 1):
