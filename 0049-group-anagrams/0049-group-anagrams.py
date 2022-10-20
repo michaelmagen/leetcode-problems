1f1c2e90ed0e1fb6530a5a84d032f99c64e1ed6a
michaelmagen <107211477+michaelmagen@users.noreply.github.com>
2022-10-20 16:32:26 -0700

    Time: 326 ms (7.50%), Space: 19.9 MB (9.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashMap = {} # sorted anagram -> array of anagrams
-
-        for n in strs:
-            anag = ''.join(sorted(n))
-            if anag in hashMap:
-                hashMap[anag].append(n)
-            else:
-                hashMap[anag] = [n]
-    
-        return hashMap.values()
+        # put every anagram in hashmap
+        answer = collections.defaultdict(list) # letters present -> array of anagrams
+        
+        for s in strs:
+            # array for each letter in alpahbet, keeps track of count of letters
+            count = [0] * 26
+            for c in s:
+                # get letter using ascii value
+                count[ord(c) - ord("a")] += 1
+            answer[tuple(count)].append(s) # need tuple bc map key cant be list
+        return answer.values()
